b049332843fe811ee8ca5532d1497e8757d90392
Daniel Couture <dcouture@fitbit.com>
2020-03-18 16:43:22 -0400

    validate that the local portion of the group email doesn't exceed 63 characters

--- gsuite/resource_group.go
+++ gsuite/resource_group.go
@@ -26,6 +26,7 @@ func resourceGroup() *schema.Resource {
 				StateFunc: func(val interface{}) string {
 					return strings.ToLower(val.(string))
 				},
+				ValidateFunc: validateEmail,
 			},
 
 			"aliases": {
@@ -61,6 +62,22 @@ func resourceGroup() *schema.Resource {
 			},
 		},
 	}
+}
+
+func validateEmail(v interface{}, k string) (warnings []string, errors []error) {
+	if v == nil || v.(string) == "" {
+		return
+	}
+	email := v.(string)
+	// TODO: This does not account for non-standard but still RFC compliant
+	// email addresses such as "some@email"@domain.com
+	local := strings.Split(email, "@")[0]
+	if len(local) > 63 {
+		errors = append(errors,
+			fmt.Errorf("local portion of email %s exceeds 63 characters", email))
+	}
+
+	return
 }
 
 func resourceGroupCreate(d *schema.ResourceData, meta interface{}) error {
