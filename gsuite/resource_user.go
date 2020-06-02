744b7f5963c3358b21ecfaa8e507e555ca1d9b4b
Romain Philibert <romain.philibert@worldline.com>
2020-06-02 09:12:52 +0200

    fmt

--- gsuite/resource_user.go
+++ gsuite/resource_user.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/pkg/errors"
+	"github.com/sethvargo/go-password/password"
 	directory "google.golang.org/api/admin/directory/v1"
 	"google.golang.org/api/googleapi"
-  "github.com/sethvargo/go-password/password"
 )
 
 func normalizeJSON(jsonString interface{}) (error, string) {
@@ -488,14 +488,14 @@ func resourceUserCreate(d *schema.ResourceData, meta interface{}) error {
 		user.Password = v.(string)
 	}
 
-  if user.Password == "" {
-    res, err := password.Generate(32, 4, 4, false, false)
-    if err != nil {
-      return err
-    }
-    user.Password = res
+	if user.Password == "" {
+		res, err := password.Generate(32, 4, 4, false, false)
+		if err != nil {
+			return err
+		}
+		user.Password = res
 		log.Printf("[INFO] A random password was generated for the user")
-  }
+	}
 
 	if v, ok := d.GetOk("hash_function"); ok {
 		log.Printf("[DEBUG] Setting %s: %s", "hash_function", v.(string))
